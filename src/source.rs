afb57304f53ff0158cea15e46baeff01bfcac247
Yashash H L <yashash_hl@intuit.com>
2023-09-22 21:41:52 +0530

    adding read
    Signed-off-by: Yashash H L <yashash_hl@intuit.com>

--- src/source.rs
+++ src/source.rs
@@ -1,10 +1,14 @@
-use tokio_stream::wrappers::ReceiverStream;
-use tonic::{async_trait, Request, Response, Status};
+use std::time::Duration;
 
+use crate::shared::prost_timestamp_from_utc;
 use crate::source::sourcer::source_server::Source;
 use crate::source::sourcer::{
     AckRequest, AckResponse, PendingResponse, ReadRequest, ReadResponse, ReadyResponse,
 };
+use chrono::{DateTime, Utc};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tonic::{async_trait, Request, Response, Status};
 
 mod sourcer {
     tonic::include_proto!("source.v1");
@@ -16,10 +20,19 @@ struct SourceService<T> {
 
 #[async_trait]
 pub trait Sourcer {
+    /// read ...
+    async fn read(&self, request: SourceReadRequest) -> Vec<Message>;
     /// Ack ...
     async fn ack(&self, offsets: Vec<Offset>);
     /// pending...
     async fn pending(&self) -> usize;
+}
+
+pub struct SourceReadRequest {
+    /// count ...
+    pub count: usize,
+    /// timeout ...
+    pub timeout: Duration,
 }
 
 /// ...
@@ -41,12 +54,52 @@ where
         &self,
         request: Request<ReadRequest>,
     ) -> Result<Response<Self::ReadFnStream>, Status> {
-        _ = request;
-        todo!()
+        let sr = request.into_inner().request.unwrap();
+
+        let responses = self
+            .handler
+            .read(SourceReadRequest {
+                count: sr.num_records as usize,
+                timeout: Duration::from_millis(sr.timeout_in_ms as u64),
+            })
+            .await;
+
+        let (tx, rx) = mpsc::channel::<Result<ReadResponse, Status>>(1);
+        tokio::spawn(async move {
+            for resp in responses {
+                tx.send(Ok(ReadResponse {
+                    result: Some(sourcer::read_response::Result {
+                        payload: resp.value,
+                        offset: Some(sourcer::Offset {
+                            offset: resp.offset.offset,
+                            partition_id: resp.offset.partition_id,
+                        }),
+                        event_time: prost_timestamp_from_utc(resp.event_time),
+                        keys: resp.keys,
+                    }),
+                }))
+                .await
+                .unwrap();
+            }
+        });
+
+        Ok(Response::new(ReceiverStream::new(rx)))
     }
 
-    async fn ack_fn(&self, _: Request<AckRequest>) -> Result<Response<AckResponse>, Status> {
-        let offsets = vec![];
+    async fn ack_fn(&self, request: Request<AckRequest>) -> Result<Response<AckResponse>, Status> {
+        let ar: AckRequest = request.into_inner();
+
+        let offsets = ar
+            .request
+            .unwrap()
+            .offsets
+            .into_iter()
+            .map(|so| Offset {
+                offset: so.offset,
+                partition_id: so.partition_id,
+            })
+            .collect();
+
         self.handler.ack(offsets).await;
 
         Ok(Response::new(AckResponse {
@@ -67,4 +120,16 @@ where
     async fn is_ready(&self, _: Request<()>) -> Result<Response<ReadyResponse>, Status> {
         Ok(Response::new(ReadyResponse { ready: true }))
     }
+}
+
+/// ...
+pub struct Message {
+    /// ...
+    pub value: Vec<u8>,
+    /// ...
+    pub offset: Offset,
+    /// ...
+    pub event_time: DateTime<Utc>,
+    /// ...
+    pub keys: Vec<String>,
 }
