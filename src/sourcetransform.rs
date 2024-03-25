9d8168677cdc329df6b5333db60bfbadb5b0b764
Vigith Maurice <vigith@gmail.com>
2024-03-24 19:29:42 -0700

    feat: make sourcetransfomer API simpler (#34)

--- src/sourcetransform.rs
+++ src/sourcetransform.rs
@@ -1,16 +1,12 @@
+use std::path::PathBuf;
+
 use chrono::{DateTime, Utc};
+use tokio::sync::oneshot;
 use tonic::{async_trait, Request, Response, Status};
 
-use crate::{
-    shared::{self, prost_timestamp_from_utc},
-    sourcetransform::sourcetransformer::SourceTransformRequest,
-};
+use crate::shared::{self, prost_timestamp_from_utc};
 
-use self::sourcetransformer::{
-    source_transform_response, source_transform_server, ReadyResponse, SourceTransformResponse,
-};
-
-mod sourcetransformer {
+mod proto {
     tonic::include_proto!("sourcetransformer.v1");
 }
 
@@ -18,59 +14,46 @@ struct SourceTransformerService<T> {
     handler: T,
 }
 
-/// SourceTransformer trait for implementing Source Transformer Handler.
+/// SourceTransformer trait for implementing SourceTransform handler.
 #[async_trait]
 pub trait SourceTransformer {
-    /// transform takes in an input element and can produce 0, 1, or more results. The input is a [`Datum`]
-    /// and the output is a ['Vec`] of [`Message`]. In a `transform` each element is processed independently
+    /// transform takes in an input element and can produce 0, 1, or more results. The input is a [`SourceTransformRequest`]
+    /// and the output is a [`Vec`] of [`Message`]. In a `transform` each element is processed independently
     /// and there is no state associated with the elements. Source transformer can be used for transforming
     /// and assigning event time to input messages. More about source transformer can be read
     /// [here](https://numaflow.numaproj.io/user-guide/sources/transformer/overview/)
     ///
     /// #Example
     ///
-    ///  ```rust,ignore
-    /// use numaflow::sourcetransform::start_uds_server;
+    /// ```no_run
+    /// use numaflow::sourcetransform;
+    /// use std::error::Error;
     ///
     /// // A simple source transformer which assigns event time to the current time in utc.
     ///
     /// #[tokio::main]
-    /// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ///    let transformer_handler = now_transformer::Now::new();
+    /// async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    ///     sourcetransform::Server::new(NowCat).start().await
+    /// }
     ///
-    ///    start_uds_server(transformer_handler).await?;
+    /// struct NowCat;
     ///
-    ///    Ok(())
-    ///}
-    ///
-    ///pub(crate) mod now_transformer {
-    ///    use numaflow::sourcetransform::{Datum, Message, SourceTransformer};
-    ///    use tonic::async_trait;
-    ///
-    ///    pub(crate) struct Now {}
-    ///
-    ///    impl Now {
-    ///        pub(crate) fn new() -> Self {
-    ///            Self {}
-    ///        }
-    ///    }
-    ///
-    ///    #[async_trait]
-    ///    impl SourceTransformer for Now {
-    ///        async fn transform<T: Datum + Send + Sync + 'static>(&self, input: T) -> Vec<Message> {
-    ///            let mut reponse = vec![];
-    ///            reponse.push(Message {
-    ///                value: input.value().clone(),
-    ///                keys: input.keys().clone(),
-    ///                tags: vec![],
-    ///                event_time: chrono::offset::Utc::now(),
-    ///            });
-    ///            reponse
-    ///        }
-    ///    }
+    /// #[tonic::async_trait]
+    /// impl sourcetransform::SourceTransformer for NowCat {
+    ///     async fn transform(
+    ///         &self,
+    ///         input: sourcetransform::SourceTransformRequest,
+    ///     ) -> Vec<sourcetransform::Message> {
+    ///         vec![sourcetransform::Message {
+    ///             keys: input.keys,
+    ///             value: input.value,
+    ///             event_time: chrono::offset::Utc::now(),
+    ///             tags: vec![],
+    ///         }]
+    ///     }
     /// }
     /// ```
-    async fn transform<T: Datum + Send + Sync + 'static>(&self, input: T) -> Vec<Message>;
+    async fn transform(&self, input: SourceTransformRequest) -> Vec<Message>;
 }
 
 /// Message is the response struct from the [`SourceTransformer::transform`] .
@@ -87,109 +70,242 @@ pub struct Message {
     pub tags: Vec<String>,
 }
 
-/// Datum trait represents an incoming element into the source_tranfrom handles of [`SourceTransformer`].
-pub trait Datum {
+/// Incoming request to the Source Transformer.
+pub struct SourceTransformRequest {
     /// keys are the keys in the (key, value) terminology of map/reduce paradigm.
     /// Once called, it will replace the content with None, so subsequent calls will return None
-    fn keys(&self) -> &Vec<String>;
+    pub keys: Vec<String>,
     /// value is the value in (key, value) terminology of map/reduce paradigm.
     /// Once called, it will replace the content with None, so subsequent calls will return None
-    fn value(&self) -> &Vec<u8>;
+    pub value: Vec<u8>,
     /// [watermark](https://numaflow.numaproj.io/core-concepts/watermarks/) represented by time is a guarantee that we will not see an element older than this
     /// time.
-    fn watermark(&self) -> DateTime<Utc>;
+    pub watermark: DateTime<Utc>,
     /// event_time is the time of the element as seen at source or aligned after a reduce operation.
-    fn event_time(&self) -> DateTime<Utc>;
+    pub eventtime: DateTime<Utc>,
 }
 
-impl Datum for OwnedSourceTransformRequest {
-    fn keys(&self) -> &Vec<String> {
-        &self.keys
-    }
-
-    fn value(&self) -> &Vec<u8> {
-        &self.value
-    }
-
-    fn watermark(&self) -> DateTime<Utc> {
-        self.watermark
-    }
-
-    fn event_time(&self) -> DateTime<Utc> {
-        self.eventtime
-    }
-}
-
-/// Owned copy of MapRequest from Datum.
-struct OwnedSourceTransformRequest {
-    keys: Vec<String>,
-    value: Vec<u8>,
-    watermark: DateTime<Utc>,
-    eventtime: DateTime<Utc>,
-}
-
-impl OwnedSourceTransformRequest {
-    fn new(str: SourceTransformRequest) -> Self {
-        Self {
-            keys: str.keys,
-            value: str.value,
-            watermark: shared::utc_from_timestamp(str.watermark),
-            eventtime: shared::utc_from_timestamp(str.event_time),
+impl From<Message> for proto::source_transform_response::Result {
+    fn from(value: Message) -> Self {
+        proto::source_transform_response::Result {
+            keys: value.keys,
+            value: value.value,
+            event_time: prost_timestamp_from_utc(value.event_time),
+            tags: value.tags,
         }
     }
 }
+
+impl From<proto::SourceTransformRequest> for SourceTransformRequest {
+    fn from(value: proto::SourceTransformRequest) -> Self {
+        Self {
+            keys: value.keys,
+            value: value.value,
+            watermark: shared::utc_from_timestamp(value.watermark),
+            eventtime: shared::utc_from_timestamp(value.event_time),
+        }
+    }
+}
+
 #[async_trait]
-impl<T> source_transform_server::SourceTransform for SourceTransformerService<T>
+impl<T> proto::source_transform_server::SourceTransform for SourceTransformerService<T>
 where
     T: SourceTransformer + Send + Sync + 'static,
 {
     async fn source_transform_fn(
         &self,
-        request: Request<SourceTransformRequest>,
-    ) -> Result<Response<SourceTransformResponse>, Status> {
+        request: Request<proto::SourceTransformRequest>,
+    ) -> Result<Response<proto::SourceTransformResponse>, Status> {
         let request = request.into_inner();
 
-        let messages = self
-            .handler
-            .transform(OwnedSourceTransformRequest::new(request))
-            .await;
+        let messages = self.handler.transform(request.into()).await;
 
-        let results = messages
-            .into_iter()
-            .map(move |msg| source_transform_response::Result {
-                keys: msg.keys,
-                event_time: prost_timestamp_from_utc(msg.event_time),
-                value: msg.value,
-                tags: msg.tags,
-            })
-            .collect::<Vec<source_transform_response::Result>>();
-
-        Ok(Response::new(SourceTransformResponse { results }))
+        Ok(Response::new(proto::SourceTransformResponse {
+            results: messages
+                .into_iter()
+                .map(|msg| msg.into())
+                .collect::<Vec<_>>(),
+        }))
     }
 
-    async fn is_ready(&self, _: Request<()>) -> Result<Response<ReadyResponse>, Status> {
-        Ok(Response::new(ReadyResponse { ready: true }))
+    async fn is_ready(&self, _: Request<()>) -> Result<Response<proto::ReadyResponse>, Status> {
+        Ok(Response::new(proto::ReadyResponse { ready: true }))
     }
 }
 
-pub async fn start_uds_server<T>(m: T) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
-where
-    T: SourceTransformer + Send + Sync + 'static,
-{
-    let server_info_file = if std::env::var_os("NUMAFLOW_POD").is_some() {
-        "/var/run/numaflow/server-info"
-    } else {
-        "/tmp/numaflow.server-info"
-    };
-    let socket_file = "/var/run/numaflow/sourcetransform.sock";
-    let listener = shared::create_listener_stream(socket_file, server_info_file)?;
-    let source_transformer_svc = SourceTransformerService { handler: m };
+/// gRPC server to start a sourcetransform service
+#[derive(Debug)]
+pub struct Server<T> {
+    sock_addr: PathBuf,
+    max_message_size: usize,
+    server_info_file: PathBuf,
+    svc: Option<T>,
+}
 
-    tonic::transport::Server::builder()
-        .add_service(source_transform_server::SourceTransformServer::new(
-            source_transformer_svc,
-        ))
-        .serve_with_incoming(listener)
-        .await
-        .map_err(Into::into)
+impl<T> Server<T> {
+    pub fn new(sourcetransformer_svc: T) -> Self {
+        let server_info_file = if std::env::var_os("NUMAFLOW_POD").is_some() {
+            "/var/run/numaflow/server-info"
+        } else {
+            "/tmp/numaflow.server-info"
+        };
+        Server {
+            sock_addr: "/var/run/numaflow/sourcetransform.sock".into(),
+            max_message_size: 64 * 1024 * 1024,
+            server_info_file: server_info_file.into(),
+            svc: Some(sourcetransformer_svc),
+        }
+    }
+
+    /// Set the unix domain socket file path used by the gRPC server to listen for incoming connections.
+    /// Default value is `/var/run/numaflow/sourcetransform.sock`
+    pub fn with_socket_file(mut self, file: impl Into<PathBuf>) -> Self {
+        self.sock_addr = file.into();
+        self
+    }
+
+    /// Get the unix domain socket file path where gRPC server listens for incoming connections. Default value is `/var/run/numaflow/XXX.sock`
+    pub fn socket_file(&self) -> &std::path::Path {
+        self.sock_addr.as_path()
+    }
+
+    /// Set the maximum size of an encoded and decoded gRPC message. The value of `message_size` is in bytes. Default value is 64MB.
+    pub fn with_max_message_size(mut self, message_size: usize) -> Self {
+        self.max_message_size = message_size;
+        self
+    }
+
+    /// Get the maximum size of an encoded and decoded gRPC message in bytes. Default value is 4MB.
+    pub fn max_message_size(&self) -> usize {
+        self.max_message_size
+    }
+
+    /// Change the file in which numflow server information is stored on start up to the new value. Default value is `/tmp/numaflow.server-info`
+    pub fn with_server_info_file(mut self, file: impl Into<PathBuf>) -> Self {
+        self.server_info_file = file.into();
+        self
+    }
+
+    /// Get the path to the file where numaflow server info is stored. Default value is `/tmp/numaflow.server-info`
+    pub fn server_info_file(&self) -> &std::path::Path {
+        self.server_info_file.as_path()
+    }
+
+    /// Starts the gRPC server. When message is received on the `shutdown` channel, graceful shutdown of the gRPC server will be initiated.
+    pub async fn start_with_shutdown(
+        &mut self,
+        shutdown: oneshot::Receiver<()>,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+    where
+        T: SourceTransformer + Send + Sync + 'static,
+    {
+        let listener = shared::create_listener_stream(&self.sock_addr, &self.server_info_file)?;
+        let handler = self.svc.take().unwrap();
+        let sourcetrf_svc = SourceTransformerService { handler };
+        let sourcetrf_svc =
+            proto::source_transform_server::SourceTransformServer::new(sourcetrf_svc)
+                .max_encoding_message_size(self.max_message_size)
+                .max_decoding_message_size(self.max_message_size);
+
+        let shutdown = async {
+            shutdown
+                .await
+                .expect("Receiving message from shutdown channel");
+        };
+        tonic::transport::Server::builder()
+            .add_service(sourcetrf_svc)
+            .serve_with_incoming_shutdown(listener, shutdown)
+            .await
+            .map_err(Into::into)
+    }
+
+    /// Starts the gRPC server. Automatically registers singal handlers for SIGINT and SIGTERM and initiates graceful shutdown of gRPC server when either one of the singal arrives.
+    pub async fn start(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+    where
+        T: SourceTransformer + Send + Sync + 'static,
+    {
+        let (tx, rx) = oneshot::channel::<()>();
+        tokio::spawn(shared::wait_for_signal(tx));
+        self.start_with_shutdown(rx).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{error::Error, time::Duration};
+    use tower::service_fn;
+
+    use crate::sourcetransform;
+    use crate::sourcetransform::proto::source_transform_client::SourceTransformClient;
+    use tempfile::TempDir;
+    use tokio::sync::oneshot;
+    use tonic::transport::Uri;
+
+    #[tokio::test]
+    async fn sourcetransformer_server() -> Result<(), Box<dyn Error>> {
+        struct NowCat;
+        #[tonic::async_trait]
+        impl sourcetransform::SourceTransformer for NowCat {
+            async fn transform(
+                &self,
+                input: sourcetransform::SourceTransformRequest,
+            ) -> Vec<sourcetransform::Message> {
+                vec![sourcetransform::Message {
+                    keys: input.keys,
+                    value: input.value,
+                    tags: vec![],
+                    event_time: chrono::offset::Utc::now(),
+                }]
+            }
+        }
+
+        let tmp_dir = TempDir::new()?;
+        let sock_file = tmp_dir.path().join("sourcetransform.sock");
+        let server_info_file = tmp_dir.path().join("server_info");
+
+        let mut server = sourcetransform::Server::new(NowCat)
+            .with_server_info_file(&server_info_file)
+            .with_socket_file(&sock_file)
+            .with_max_message_size(10240);
+
+        assert_eq!(server.max_message_size(), 10240);
+        assert_eq!(server.server_info_file(), server_info_file);
+        assert_eq!(server.socket_file(), sock_file);
+
+        let (shutdown_tx, shutdown_rx) = oneshot::channel();
+        let task = tokio::spawn(async move { server.start_with_shutdown(shutdown_rx).await });
+
+        tokio::time::sleep(Duration::from_millis(50)).await;
+
+        // https://github.com/hyperium/tonic/blob/master/examples/src/uds/client.rs
+        let channel = tonic::transport::Endpoint::try_from("http://[::]:50051")?
+            .connect_with_connector(service_fn(move |_: Uri| {
+                // Connect to a Uds socket
+                let sock_file = sock_file.clone();
+                tokio::net::UnixStream::connect(sock_file)
+            }))
+            .await?;
+
+        let mut client = SourceTransformClient::new(channel);
+        let request = tonic::Request::new(sourcetransform::proto::SourceTransformRequest {
+            keys: vec!["first".into(), "second".into()],
+            value: "hello".into(),
+            watermark: Some(prost_types::Timestamp::default()),
+            event_time: Some(prost_types::Timestamp::default()),
+        });
+
+        let resp = client.source_transform_fn(request).await?;
+        let resp = resp.into_inner();
+        assert_eq!(resp.results.len(), 1, "Expected single message from server");
+        let msg = &resp.results[0];
+        assert_eq!(msg.keys.first(), Some(&"first".to_owned()));
+        assert_eq!(msg.value, "hello".as_bytes());
+
+        shutdown_tx
+            .send(())
+            .expect("Sending shutdown signal to gRPC server");
+        tokio::time::sleep(Duration::from_millis(50)).await;
+        assert!(task.is_finished(), "gRPC server is still running");
+        Ok(())
+    }
 }
