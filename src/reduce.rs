e8a6997184d91c4d06461d5eff73d415b198744e
Yashash H L <yashashhl25@gmail.com>
2024-05-06 09:31:15 +0530

    chore: simplified reducer and side input api  (#39)
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- src/reduce.rs
+++ src/reduce.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use std::future::Future;
+use std::path::PathBuf;
 use std::sync::Arc;
 
 use chrono::{DateTime, TimeZone, Utc};
@@ -8,16 +10,18 @@ use tokio::task::JoinSet;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::metadata::MetadataMap;
 use tonic::{async_trait, Request, Response, Status};
-
-use crate::reduce::reducer::{
-    reduce_response, reduce_server, ReadyResponse, ReduceRequest, ReduceResponse,
-};
 use crate::shared;
 
-use self::reducer::reduce_server::Reduce;
+const KEY_JOIN_DELIMITER: &str = ":";
+// grpc window metadata
+const WIN_START_TIME: &str = "x-numaflow-win-start-time";
+const WIN_END_TIME: &str = "x-numaflow-win-end-time";
+const DEFAULT_MAX_MESSAGE_SIZE: usize = 64 * 1024 * 1024;
+const DEFAULT_SOCK_ADDR: &str = "/var/run/numaflow/reduce.sock";
+const DEFAULT_SERVER_INFO_FILE: &str = "/var/run/numaflow/reducer-server-info";
 
 /// Numaflow Reduce Proto definitions.
-pub mod reducer {
+pub mod proto {
     tonic::include_proto!("reduce.v1");
 }
 
@@ -25,7 +29,7 @@ struct ReduceService<T> {
     handler: Arc<T>,
 }
 
-/// Trait implemented Reduce reduce handler.
+/// Reducer trait for implementing Reduce handler.
 #[async_trait]
 pub trait Reducer {
     /// reduce_handle is provided with a set of keys, a channel of [`Datum`], and [`Metadata`]. It
@@ -37,19 +41,21 @@ pub trait Reducer {
     ///
     /// Below is a reduce code to count the number of elements for a given set of keys and window.
     ///
-    /// ```rust,ignore
-    /// use numaflow::reduce::start_uds_server;
+    /// ```no_run
+    /// use numaflow::reduce;
+    ///
     /// #[tokio::main]
-    /// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ///     let reduce_handler = counter::Counter::new();
-    ///     start_uds_server(reduce_handler).await?;
+    /// async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    /// let reduce_handler = counter::Counter::new();
+    ///     reduce::Server::new(reduce_handler).start().await?;
     ///     Ok(())
     /// }
     /// mod counter {
-    ///     use numaflow::reduce::{Datum, Message};
+    ///     use numaflow::reduce::{Message, ReduceRequest};
     ///     use numaflow::reduce::{Reducer, Metadata};
     ///     use tokio::sync::mpsc::Receiver;
     ///     use tonic::async_trait;
+    /// use numaflow::reduce::proto::reduce_server::Reduce;
     ///     pub(crate) struct Counter {}
     ///     impl Counter {
     ///         pub(crate) fn new() -> Self {
@@ -58,14 +64,11 @@ pub trait Reducer {
     ///     }
     ///     #[async_trait]
     ///     impl Reducer for Counter {
-    ///         async fn reduce<
-    ///             T: Datum + Send + Sync + 'static,
-    ///             U: Metadata + Send + Sync + 'static,
-    ///         >(
+    ///         async fn reduce(
     ///             &self,
     ///             keys: Vec<String>,
-    ///             mut input: Receiver<T>,
-    ///             md: &U,
+    ///             mut input: Receiver<ReduceRequest>,
+    ///             md: Metadata,
     ///         ) -> Vec<Message> {
     ///             let mut counter = 0;
     ///             // the loop exits when input is closed which will happen only on close of book.
@@ -82,44 +85,39 @@ pub trait Reducer {
     /// }
     ///```
     /// [Window]: https://numaflow.numaproj.io/user-guide/user-defined-functions/reduce/windowing/windowing/
-    async fn reduce<T: Datum + Send + Sync + 'static, U: Metadata + Send + Sync + 'static>(
+    async fn reduce(
         &self,
         keys: Vec<String>,
-        input: mpsc::Receiver<T>,
-        md: &U,
+        input: mpsc::Receiver<ReduceRequest>,
+        md: Metadata,
     ) -> Vec<Message>;
 }
 
 /// IntervalWindow is the start and end boundary of the window.
-struct IntervalWindow {
-    // st is start time
-    st: DateTime<Utc>,
-    // et is end time
-    et: DateTime<Utc>,
+#[derive(Clone)]
+pub struct IntervalWindow {
+    // start time of the window
+    pub start_time: DateTime<Utc>,
+    // end time of the window
+    pub end_time: DateTime<Utc>,
 }
 
 impl IntervalWindow {
-    fn new(st: DateTime<Utc>, et: DateTime<Utc>) -> Self {
-        Self { st, et }
+    fn new(start_time: DateTime<Utc>, end_time: DateTime<Utc>) -> Self {
+        Self { start_time, end_time }
     }
 }
 
+impl Metadata {
+    pub fn new(interval_window: IntervalWindow) -> Self {
+        Self { interval_window }
+    }
+}
+
+#[derive(Clone)]
 /// Metadata are additional information passed into the [`Reducer::reduce`].
-pub trait Metadata {
-    /// start_time is the window start time.
-    fn start_time(&self) -> &DateTime<Utc>;
-    /// end_time is the window end time.
-    fn end_time(&self) -> &DateTime<Utc>;
-}
-
-impl Metadata for IntervalWindow {
-    fn start_time(&self) -> &DateTime<Utc> {
-        &self.st
-    }
-
-    fn end_time(&self) -> &DateTime<Utc> {
-        &self.et
-    }
+pub struct Metadata {
+    pub interval_window: IntervalWindow
 }
 
 /// Message is the response from the user's [`Reducer::reduce`].
@@ -133,29 +131,20 @@ pub struct Message {
     pub tags: Vec<String>,
 }
 
-/// Datum trait represents an incoming element into the reduce handle of [`Reducer`].
-pub trait Datum {
-    /// keys are the keys in the (key, value) terminology of map/reduce paradigm.
-    fn keys(&self) -> &Vec<String>;
-    /// value is the value in (key, value) terminology of map/reduce paradigm.
-    fn value(&self) -> &Vec<u8>;
-    /// [watermark](https://numaflow.numaproj.io/core-concepts/watermarks/) represented by time is a guarantee that we will not see an element older than this
-    /// time.
-    fn watermark(&self) -> DateTime<Utc>;
-    /// event_time is the time of the element as seen at source or aligned after a reduce operation.
-    fn event_time(&self) -> DateTime<Utc>;
+/// Incoming request into the reducer handler of [`Reducer`].
+pub struct ReduceRequest {
+    /// Set of keys in the (key, value) terminology of map/reduce paradigm.
+    pub keys: Vec<String>,
+    /// The value in the (key, value) terminology of map/reduce paradigm.    /// The value in the (key, value) terminology of map/reduce paradigm.
+    pub value: Vec<u8>,
+    /// [watermark](https://numaflow.numaproj.io/core-concepts/watermarks/) represented by time is a guarantee that we will not see an element older than this time.    /// [watermark](https://numaflow.numaproj.io/core-concepts/watermarks/) represented by time is a guarantee that we will not see an element older than this time.
+    pub watermark: DateTime<Utc>,
+    /// Time of the element as seen at source or aligned after a reduce operation.
+    pub eventtime: DateTime<Utc>,
 }
 
-/// Owned copy of ReduceRequest from Datum.
-struct OwnedReduceRequest {
-    keys: Vec<String>,
-    value: Vec<u8>,
-    watermark: DateTime<Utc>,
-    eventtime: DateTime<Utc>,
-}
-
-impl OwnedReduceRequest {
-    fn new(mr: ReduceRequest) -> Self {
+impl From<proto::ReduceRequest> for ReduceRequest {
+    fn from(mr: proto::ReduceRequest) -> Self {
         Self {
             keys: mr.keys,
             value: mr.value,
@@ -164,30 +153,6 @@ impl OwnedReduceRequest {
         }
     }
 }
-
-impl Datum for OwnedReduceRequest {
-    fn keys(&self) -> &Vec<String> {
-        &self.keys
-    }
-
-    fn value(&self) -> &Vec<u8> {
-        &self.value
-    }
-
-    fn watermark(&self) -> DateTime<Utc> {
-        self.watermark
-    }
-
-    fn event_time(&self) -> DateTime<Utc> {
-        self.eventtime
-    }
-}
-
-// key delimiter
-const KEY_JOIN_DELIMITER: &str = ":";
-// grpc window metadata
-const WIN_START_TIME: &str = "x-numaflow-win-start-time";
-const WIN_END_TIME: &str = "x-numaflow-win-end-time";
 
 // extract start and end time from the gRPC MetadataMap
 // https://youtu.be/s5S2Ed5T-dc?t=662
@@ -217,47 +182,47 @@ fn get_window_details(request: &MetadataMap) -> (DateTime<Utc>, DateTime<Utc>) {
 }
 
 #[async_trait]
-impl<T> Reduce for ReduceService<T>
+impl<T> proto::reduce_server::Reduce for ReduceService<T>
 where
     T: Reducer + Send + Sync + 'static,
 {
-    type ReduceFnStream = ReceiverStream<Result<ReduceResponse, Status>>;
+    type ReduceFnStream = ReceiverStream<Result<proto::ReduceResponse, Status>>;
     async fn reduce_fn(
         &self,
-        request: Request<tonic::Streaming<ReduceRequest>>,
+        request: Request<tonic::Streaming<proto::ReduceRequest>>,
     ) -> Result<Response<Self::ReduceFnStream>, Status> {
         // get gRPC window from metadata
         let (start_win, end_win) = get_window_details(request.metadata());
-        let md = Arc::new(IntervalWindow::new(start_win, end_win));
+        let md = Metadata::new(IntervalWindow::new(start_win, end_win));
 
-        let mut key_to_tx: HashMap<String, Sender<OwnedReduceRequest>> = HashMap::new();
+        let mut key_to_tx: HashMap<String, Sender<ReduceRequest>> = HashMap::new();
 
         // we will be creating a set of tasks for this stream
         let mut set = JoinSet::new();
 
         let mut stream = request.into_inner();
 
-        while let Some(datum) = stream.message().await.unwrap() {
-            let task_name = datum.keys.join(KEY_JOIN_DELIMITER);
+        while let Some(rr) = stream.message().await.unwrap() {
+            let task_name = rr.keys.join(KEY_JOIN_DELIMITER);
 
             if let Some(tx) = key_to_tx.get(&task_name) {
-                tx.send(OwnedReduceRequest::new(datum)).await.unwrap();
+                tx.send(rr.into()).await.unwrap();
             } else {
                 // channel to send data to the user's reduce handle
-                let (tx, rx) = mpsc::channel::<OwnedReduceRequest>(1);
+                let (tx, rx) = mpsc::channel::<ReduceRequest>(1);
 
                 // since we are calling this in a loop, we need make sure that there is reference counting
                 // and the lifetime of self is more than the async function.
                 // try Arc<Self> https://doc.rust-lang.org/reference/items/associated-items.html#methods ?
                 let v = Arc::clone(&self.handler);
-                let m = Arc::clone(&md);
 
                 // spawn task for each unique key
-                let keys = datum.keys.clone();
-                set.spawn(async move { v.reduce(keys, rx, m.as_ref()).await });
+                let keys = rr.keys.clone();
+                let reduce_md = md.clone();
+                set.spawn(async move { v.reduce(keys, rx, reduce_md).await });
 
                 // write data into the channel
-                tx.send(OwnedReduceRequest::new(datum)).await.unwrap();
+                tx.send(rr.into()).await.unwrap();
 
                 // save the key and for future look up as long as the stream is active
                 key_to_tx.insert(task_name, tx);
@@ -268,7 +233,7 @@ where
         key_to_tx.clear();
 
         // channel to respond to numaflow main car as it expects streaming results.
-        let (tx, rx) = mpsc::channel::<Result<ReduceResponse, Status>>(1);
+        let (tx, rx) = mpsc::channel::<Result<proto::ReduceResponse, Status>>(1);
 
         // start the result streamer
         tokio::spawn(async move {
@@ -276,14 +241,14 @@ where
                 let messages = res.unwrap();
                 let mut datum_responses = vec![];
                 for message in messages {
-                    datum_responses.push(reduce_response::Result {
+                    datum_responses.push(proto::reduce_response::Result {
                         keys: message.keys,
                         value: message.value,
                         tags: message.tags,
                     });
                 }
                 // stream it out to the client
-                tx.send(Ok(ReduceResponse {
+                tx.send(Ok(proto::ReduceResponse {
                     results: datum_responses,
                 }))
                 .await
@@ -295,25 +260,93 @@ where
         Ok(Response::new(ReceiverStream::new(rx)))
     }
 
-    async fn is_ready(&self, _: Request<()>) -> Result<Response<ReadyResponse>, Status> {
-        Ok(Response::new(ReadyResponse { ready: true }))
+    async fn is_ready(&self, _: Request<()>) -> Result<Response<proto::ReadyResponse>, Status> {
+        Ok(Response::new(proto::ReadyResponse { ready: true }))
     }
 }
 
-pub async fn start_uds_server<T>(m: T) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
-where
-    T: Reducer + Send + Sync + 'static,
-{
-    let socket_file = "/var/run/numaflow/reduce.sock";
-    let listener =
-        shared::create_listener_stream(socket_file, "/var/run/numaflow/reducer-server-info")?;
-    let reduce_svc = ReduceService {
-        handler: Arc::new(m),
-    };
+/// gRPC server to start a reduce service
+#[derive(Debug)]
+pub struct Server<T> {
+    sock_addr: PathBuf,
+    max_message_size: usize,
+    server_info_file: PathBuf,
+    svc: Option<T>,
+}
 
-    tonic::transport::Server::builder()
-        .add_service(reduce_server::ReduceServer::new(reduce_svc))
-        .serve_with_incoming(listener)
-        .await
-        .map_err(Into::into)
+impl<T> Server<T> {
+    /// Create a new Server with the given reduce service
+    pub fn new(reduce_svc: T) -> Self {
+        Server {
+            sock_addr: DEFAULT_SOCK_ADDR.into(),
+            max_message_size: DEFAULT_MAX_MESSAGE_SIZE,
+            server_info_file: DEFAULT_SERVER_INFO_FILE.into(),
+            svc: Some(reduce_svc),
+        }
+    }
+
+    /// Set the unix domain socket file path used by the gRPC server to listen for incoming connections.
+    /// Default value is `/var/run/numaflow/reduce.sock`
+    pub fn with_socket_file(mut self, file: impl Into<PathBuf>) -> Self {
+        self.sock_addr = file.into();
+        self
+    }
+
+    /// Get the unix domain socket file path where gRPC server listens for incoming connections. Default value is `/var/run/numaflow/reduce.sock`
+    pub fn socket_file(&self) -> &std::path::Path {
+        self.sock_addr.as_path()
+    }
+
+    /// Set the maximum size of an encoded and decoded gRPC message. The value of `message_size` is in bytes. Default value is 64MB.
+    pub fn with_max_message_size(mut self, message_size: usize) -> Self {
+        self.max_message_size = message_size;
+        self
+    }
+
+    /// Get the maximum size of an encoded and decoded gRPC message in bytes. Default value is 64MB.
+    pub fn max_message_size(&self) -> usize {
+        self.max_message_size
+    }
+
+    /// Change the file in which numaflow server information is stored on start up to the new value. Default value is `/var/run/numaflow/reducer-server-info`
+    pub fn with_server_info_file(mut self, file: impl Into<PathBuf>) -> Self {
+        self.server_info_file = file.into();
+        self
+    }
+
+    /// Get the path to the file where numaflow server info is stored. Default value is `/var/run/numaflow/reducer-server-info`
+    pub fn server_info_file(&self) -> &std::path::Path {
+        self.server_info_file.as_path()
+    }
+
+    /// Starts the gRPC server. When message is received on the `shutdown` channel, graceful shutdown of the gRPC server will be initiated.
+    pub async fn start_with_shutdown<F>(
+        &mut self,
+        shutdown: F,
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+        where
+            T: Reducer + Send + Sync + 'static,
+            F: Future<Output = ()>,
+    {
+        let listener = shared::create_listener_stream(&self.sock_addr, &self.server_info_file)?;
+        let handler = Arc::new(self.svc.take().unwrap());
+        let reduce_svc = ReduceService { handler };
+        let reduce_svc = proto::reduce_server::ReduceServer::new(reduce_svc)
+            .max_encoding_message_size(self.max_message_size)
+            .max_decoding_message_size(self.max_message_size);
+
+        tonic::transport::Server::builder()
+            .add_service(reduce_svc)
+            .serve_with_incoming_shutdown(listener, shutdown)
+            .await
+            .map_err(Into::into)
+    }
+
+    /// Starts the gRPC server. Automatically registers signal handlers for SIGINT and SIGTERM and initiates graceful shutdown of gRPC server when either one of the signal arrives.
+    pub async fn start(&mut self) -> Result<(), Box<dyn std::error::Error + Send + Sync>>
+        where
+            T: Reducer + Send + Sync + 'static,
+    {
+        self.start_with_shutdown(shared::shutdown_signal()).await
+    }
 }
