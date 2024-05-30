aa3da61ae5cf7e940ab38a9a7e8b34c7ab8f31c5
Yashash H L <yashashhl25@gmail.com>
2024-05-30 21:37:33 +0530

    remove from map
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- src/reduce.rs
+++ src/reduce.rs
@@ -528,10 +528,15 @@ where
     }
 
     /// Closes all tasks in the task manager and sends an EOF message to the response stream.
-    // Method to close all tasks
     async fn close_all_tasks(&mut self) {
-        for task in self.tasks..values_mut() {
-            task.close().await;
+        let task_keys: Vec<_> = self.tasks.keys().cloned().collect();
+        for task_key in task_keys {
+            if let Some(task) = self.tasks.remove(&task_key) {
+                // drop the sender to close the task
+                drop(task.tx);
+                // wait for the task to finish
+                task.handle.await.unwrap();
+            }
         }
 
         // after all the tasks have been closed, send an EOF message to the response stream
