811486667827d9e047e9cd29db89dbeee4c3181e
shubham <shubhamdixit863@gmail.com>
2024-06-12 19:05:14 +0530

    Fixed event filtering tests
    Signed-off-by: shubham <shubhamdixit863@gmail.com>

--- examples/source-transformer-event-time-filter/src/main.rs
+++ examples/source-transformer-event-time-filter/src/main.rs
@@ -1,14 +1,11 @@
-use std::error::Error;
+use filter_impl::filter_event_time;
 use numaflow::sourcetransform;
 use numaflow::sourcetransform::{Message, SourceTransformRequest};
-use filter_impl::filter_event_time;
+use std::error::Error;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     sourcetransform::Server::new(EventTimeFilter).start().await
 }
-
-
-
 
 struct EventTimeFilter;
 #[tonic::async_trait]
@@ -19,74 +16,75 @@ impl sourcetransform::SourceTransformer for EventTimeFilter {
 }
 
 mod filter_impl {
-    use numaflow::sourcetransform::{Message, SourceTransformRequest};
     use chrono::{TimeZone, Utc};
-    pub  fn filter_event_time(input: SourceTransformRequest) ->Vec<Message>{
+    use numaflow::sourcetransform::{Message, SourceTransformRequest};
+    pub fn filter_event_time(input: SourceTransformRequest) -> Vec<Message> {
         let jan_first_2022 = Utc.with_ymd_and_hms(2022, 1, 1, 0, 0, 0).unwrap();
         let jan_first_2023 = Utc.with_ymd_and_hms(2023, 1, 1, 0, 0, 0).unwrap();
         if input.eventtime < jan_first_2022 {
-            vec![ Message::message_to_drop(input.eventtime)]
+            vec![Message::message_to_drop(input.eventtime)]
         } else if input.eventtime < jan_first_2023 {
-            vec![Message::new(input.value,jan_first_2022).tags(vec![String::from("within_year_2022")])]
+            vec![Message::new(input.value, jan_first_2022)
+                .tags(vec![String::from("within_year_2022")])]
         } else {
-            vec![Message::new(input.value,jan_first_2023).tags(vec![String::from("after_year_2022")])]
+            vec![Message::new(input.value, jan_first_2023)
+                .tags(vec![String::from("after_year_2022")])]
         }
     }
-
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use crate::filter_impl::filter_event_time;
     use chrono::{TimeZone, Utc};
     use numaflow::sourcetransform::{Message, SourceTransformRequest};
     #[test]
-    fn test_filter_event_time_should_return_after_year_2022(){
+    fn test_filter_event_time_should_return_after_year_2022() {
         let time = Utc.with_ymd_and_hms(2022, 7, 2, 2, 0, 0).unwrap();
 
-        let source_request =SourceTransformRequest{
+        let source_request = SourceTransformRequest {
             keys: vec![],
             value: vec![],
             watermark: Default::default(),
             eventtime: time,
             headers: Default::default(),
         };
-        let messages=filter_event_time(source_request);
-        assert_eq!((&messages).len(),1);
+        let messages = filter_event_time(source_request);
+        assert_eq!((&messages).len(), 1);
 
-        assert_eq!((&messages)[0].tags.as_ref().unwrap()[0],"within_year_2022")
+        assert_eq!((&messages)[0].tags.as_ref().unwrap()[0], "within_year_2022")
     }
 
     #[test]
-    fn test_filter_event_time_should_return_within_year_2022(){
+    fn test_filter_event_time_should_return_within_year_2022() {
         let time = Utc.with_ymd_and_hms(2023, 7, 2, 2, 0, 0).unwrap();
 
-        let source_request =SourceTransformRequest{
+        let source_request = SourceTransformRequest {
             keys: vec![],
             value: vec![],
             watermark: Default::default(),
             eventtime: time,
             headers: Default::default(),
         };
-        let messages=filter_event_time(source_request);
-        assert_eq!((&messages).len(),1);
+        let messages = filter_event_time(source_request);
+        assert_eq!((&messages).len(), 1);
 
-        assert_eq!((&messages)[0].tags.as_ref().unwrap()[0],"after_year_2022")
+        assert_eq!((&messages)[0].tags.as_ref().unwrap()[0], "after_year_2022")
     }
 
     #[test]
-    fn test_filter_event_time_should_drop(){
+    fn test_filter_event_time_should_drop() {
         let time = Utc.with_ymd_and_hms(2021, 7, 2, 2, 0, 0).unwrap();
 
-        let source_request =SourceTransformRequest{
+        let source_request = SourceTransformRequest {
             keys: vec![],
             value: vec![],
             watermark: Default::default(),
             eventtime: time,
             headers: Default::default(),
         };
-        let messages=filter_event_time(source_request);
-        assert_eq!((&messages).len(),1);
-        assert_eq!((&messages)[0].tags.as_ref().unwrap()[0],"U+005C__DROP__")
+        let messages = filter_event_time(source_request);
+        assert_eq!((&messages).len(), 1);
+        assert_eq!((&messages)[0].tags.as_ref().unwrap()[0], "U+005C__DROP__")
     }
 }
