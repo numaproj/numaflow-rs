7288b17b078c72d7686ca907b2277583dc938ceb
Shubham Dixit <shubhamdixit863@gmail.com>
2024-06-15 12:13:05 +0530

    Fixed the utc_from_timestamp method ,added test cases (#60)

--- src/shared.rs
+++ src/shared.rs
@@ -43,11 +43,11 @@ pub(crate) fn create_listener_stream(
 }
 
 pub(crate) fn utc_from_timestamp(t: Option<Timestamp>) -> DateTime<Utc> {
-    if let Some(ref t) = t {
-        Utc.timestamp_nanos(t.seconds * (t.nanos as i64))
-    } else {
-        Utc.timestamp_nanos(-1)
-    }
+    t.map_or(Utc.timestamp_nanos(-1), |t| {
+
+        DateTime::from_timestamp(t.seconds, t.nanos as u32).unwrap_or(Utc.timestamp_nanos(-1))
+
+    })
 }
 
 pub(crate) fn prost_timestamp_from_utc(t: DateTime<Utc>) -> Option<Timestamp> {
@@ -74,4 +74,50 @@ pub(crate) async fn shutdown_signal() {
         _ = ctrl_c => {},
         _ = terminate => {},
     }
+}
+#[cfg(test)]
+mod tests{
+    use super::*;
+    #[test]
+    fn test_utc_from_timestamp(){
+        let specific_date = Utc.with_ymd_and_hms(2022, 7, 2, 2, 0, 0).unwrap();
+
+        let timestamp = Timestamp {
+            seconds: specific_date.timestamp(),
+            nanos: specific_date.timestamp_subsec_nanos() as i32,
+        };
+
+        let utc_ts=utc_from_timestamp(Some(timestamp));
+        assert_eq!(utc_ts,specific_date)
+    }
+    #[test]
+    fn test_utc_from_timestamp_epoch_0(){
+        let specific_date = Utc.timestamp_nanos(-1);
+
+        let utc_ts=utc_from_timestamp(None);
+        assert_eq!(utc_ts,specific_date)
+    }
+    #[test]
+    fn test_prost_timestamp_from_utc(){
+        let specific_date = Utc.with_ymd_and_hms(2022, 7, 2, 2, 0, 0).unwrap();
+        let timestamp = Timestamp {
+            seconds: specific_date.timestamp(),
+            nanos: specific_date.timestamp_subsec_nanos() as i32,
+        };
+        let prost_ts=prost_timestamp_from_utc(specific_date);
+        assert_eq!(prost_ts,Some(timestamp))
+    }
+
+    #[test]
+    fn test_prost_timestamp_from_utc_epoch_0() {
+        let specific_date = Utc.timestamp(0, 0);
+        let timestamp = Timestamp {
+            seconds: 0,
+            nanos: 0,
+        };
+        let prost_ts = prost_timestamp_from_utc(specific_date);
+        assert_eq!(prost_ts, Some(timestamp));
+    }
+
+
 }
