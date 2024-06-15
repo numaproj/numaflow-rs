c14ea2b964feb7646f9e443de756f312d1851947
shubham <shubhamdixit863@gmail.com>
2024-06-15 12:09:06 +0530

    added test case for epoch 0
    Signed-off-by: shubham <shubhamdixit863@gmail.com>

--- src/shared.rs
+++ src/shared.rs
@@ -91,6 +91,13 @@ mod tests{
         assert_eq!(utc_ts,specific_date)
     }
     #[test]
+    fn test_utc_from_timestamp_epoch_0(){
+        let specific_date = Utc.timestamp_nanos(-1);
+
+        let utc_ts=utc_from_timestamp(None);
+        assert_eq!(utc_ts,specific_date)
+    }
+    #[test]
     fn test_prost_timestamp_from_utc(){
         let specific_date = Utc.with_ymd_and_hms(2022, 7, 2, 2, 0, 0).unwrap();
         let timestamp = Timestamp {
@@ -100,5 +107,17 @@ mod tests{
         let prost_ts=prost_timestamp_from_utc(specific_date);
         assert_eq!(prost_ts,Some(timestamp))
     }
+
+    #[test]
+    fn test_prost_timestamp_from_utc_epoch_0() {
+        let specific_date = Utc.timestamp(0, 0);
+        let timestamp = Timestamp {
+            seconds: 0,
+            nanos: 0,
+        };
+        let prost_ts = prost_timestamp_from_utc(specific_date);
+        assert_eq!(prost_ts, Some(timestamp));
+    }
+
 
 }
