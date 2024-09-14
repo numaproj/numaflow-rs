8e170f4ef412eb147ea6be91cd95413ab1afdb16
Yashash H L <yashashhl25@gmail.com>
2024-09-14 07:06:53 +0530

    feat: Bidirectional Streaming for Source (#84)

--- src/source.rs
+++ src/source.rs
@@ -4,17 +4,24 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::Duration;
 
+use crate::error::Error::SourceError;
+use crate::error::{Error, ErrorKind};
 use crate::shared::{self, prost_timestamp_from_utc};
+use crate::source::proto::{AckRequest, AckResponse, ReadRequest, ReadResponse};
 use chrono::{DateTime, Utc};
-use tokio::sync::mpsc::{self, Sender};
+use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio::sync::oneshot;
+use tokio::task::JoinHandle;
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_util::sync::CancellationToken;
-use tonic::{async_trait, Request, Response, Status};
+use tonic::{async_trait, Request, Response, Status, Streaming};
+use tracing::{error, info};
 
 const DEFAULT_MAX_MESSAGE_SIZE: usize = 64 * 1024 * 1024;
 const DEFAULT_SOCK_ADDR: &str = "/var/run/numaflow/source.sock";
 const DEFAULT_SERVER_INFO_FILE: &str = "/var/run/numaflow/sourcer-server-info";
+// TODO: use batch-size, blocked by https://github.com/numaproj/numaflow/issues/2026
+const DEFAULT_CHANNEL_SIZE: usize = 1000;
 
 /// Source Proto definitions.
 pub mod proto {
@@ -23,10 +30,11 @@ pub mod proto {
 
 struct SourceService<T> {
     handler: Arc<T>,
-    _shutdown_tx: Sender<()>,
-    _cancellation_token: CancellationToken,
+    shutdown_tx: Sender<()>,
+    cancellation_token: CancellationToken,
 }
 
+// FIXME: remove async_trait
 #[async_trait]
 /// Trait representing a [user defined source](https://numaflow.numaproj.io/user-guide/sources/overview/).
 ///
@@ -46,8 +54,8 @@ struct SourceService<T> {
 pub trait Sourcer {
     /// Reads the messages from the source and sends them to the transmitter.
     async fn read(&self, request: SourceReadRequest, transmitter: Sender<Message>);
-    /// Acknowledges the messages that have been processed by the user-defined source.
-    async fn ack(&self, offsets: Vec<Offset>);
+    /// Acknowledges the message that has been processed by the user-defined source.
+    async fn ack(&self, offset: Offset);
     /// Returns the number of messages that are yet to be processed by the user-defined source.
     async fn pending(&self) -> usize;
     /// Returns the partitions associated with the source. This will be used by the platform to determine
@@ -73,29 +81,21 @@ pub struct Offset {
     pub partition_id: i32,
 }
 
-#[async_trait]
-impl<T> proto::source_server::Source for SourceService<T>
+impl<T> SourceService<T>
 where
     T: Sourcer + Send + Sync + 'static,
 {
-    type ReadFnStream = ReceiverStream<Result<proto::ReadResponse, Status>>;
-
-    async fn read_fn(
-        &self,
-        request: Request<proto::ReadRequest>,
-    ) -> Result<Response<Self::ReadFnStream>, Status> {
-        let sr = request.into_inner().request.unwrap();
-
-        // tx,rx pair for sending data over to user-defined source
-        let (stx, mut srx) = mpsc::channel::<Message>(sr.num_records as usize);
-        // tx,rx pair for gRPC response
-        let (tx, rx) =
-            mpsc::channel::<Result<proto::ReadResponse, Status>>(sr.num_records as usize);
-
-        // start the ud-source rx asynchronously and start populating the gRPC response, so it can be streamed to the gRPC client (numaflow).
-        tokio::spawn(async move {
-            while let Some(resp) = srx.recv().await {
-                tx.send(Ok(proto::ReadResponse {
+    /// writes a read batch returned by the user-defined handler to the client (numaflow).
+    async fn write_a_batch(
+        grpc_resp_tx: Sender<Result<ReadResponse, Status>>,
+        mut udsource_rx: Receiver<Message>,
+    ) -> crate::error::Result<()> {
+        // even though we use bi-di; the user-defined source sees this as a 1/2 duplex
+        // server side streaming. this means that the below while loop will terminate
+        // after every batch of read has been returned.
+        while let Some(resp) = udsource_rx.recv().await {
+            grpc_resp_tx
+                .send(Ok(ReadResponse {
                     result: Some(proto::read_response::Result {
                         payload: resp.value,
                         offset: Some(proto::Offset {
@@ -104,26 +104,139 @@ where
                         }),
                         event_time: prost_timestamp_from_utc(resp.event_time),
                         keys: resp.keys,
+                        headers: Default::default(),
                     }),
+                    status: None,
                 }))
                 .await
-                .expect("receiver dropped");
+                .map_err(|e| SourceError(ErrorKind::InternalError(e.to_string())))?;
+        }
+
+        // send end of transmission on success
+        grpc_resp_tx
+            .send(Ok(ReadResponse {
+                result: None,
+                status: Some(proto::read_response::Status {
+                    eot: true,
+                    code: 0,
+                    error: 0,
+                    msg: None,
+                }),
+            }))
+            .await
+            .map_err(|e| SourceError(ErrorKind::InternalError(e.to_string())))?;
+
+        Ok(())
+    }
+
+    /// Invokes the user-defined source handler to get a read batch and streams it to the numaflow
+    /// (client).
+    async fn forward_a_batch(
+        handler_fn: Arc<T>,
+        grpc_resp_tx: Sender<Result<ReadResponse, Status>>,
+        request: proto::read_request::Request,
+    ) -> crate::error::Result<()> {
+        // tx,rx pair for sending data over to user-defined source
+        let (stx, srx) = mpsc::channel::<Message>(DEFAULT_CHANNEL_SIZE);
+
+        // spawn the rx side so that when the handler is invoked, we can stream the handler's read data
+        // to the gprc response stream.
+        let grpc_writer_handle: JoinHandle<Result<(), Error>> =
+            tokio::spawn(async move { Self::write_a_batch(grpc_resp_tx, srx).await });
+
+        // spawn the handler, it will stream the data to tx passed which will be streamed to the client
+        // by the above task.
+        handler_fn
+            .read(
+                SourceReadRequest {
+                    count: request.num_records as usize,
+                    timeout: Duration::from_millis(request.timeout_in_ms as u64),
+                },
+                stx,
+            )
+            .await;
+
+        // wait for the spawned grpc writer to end
+        grpc_writer_handle
+            .await
+            .map_err(|e| SourceError(ErrorKind::InternalError(e.to_string())))?
+            .map_err(|e| SourceError(ErrorKind::InternalError(e.to_string())))?;
+
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl<T> proto::source_server::Source for SourceService<T>
+where
+    T: Sourcer + Send + Sync + 'static,
+{
+    type ReadFnStream = ReceiverStream<Result<ReadResponse, Status>>;
+
+    async fn read_fn(
+        &self,
+        request: Request<Streaming<ReadRequest>>,
+    ) -> Result<Response<Self::ReadFnStream>, Status> {
+        let mut sr = request.into_inner();
+        // we have to call the handler over and over for each ReadRequest
+        let handler_fn = Arc::clone(&self.handler);
+
+        // tx (read from client), rx (write to client) pair for gRPC response
+        let (tx, rx) = mpsc::channel::<Result<ReadResponse, Status>>(DEFAULT_CHANNEL_SIZE);
+
+        // this _tx ends up writing to the client side
+        let grpc_tx = tx.clone();
+
+        let cln_token = self.cancellation_token.clone();
+
+        // this is the top-level stream consumer and this task will only exit when stream is closed (which
+        // will happen when server and client are shutting down).
+        let grpc_read_handle: JoinHandle<Result<(), Error>> = tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    // for each ReadRequest message, the handler will be called and a batch of messages
+                    // will be sent over to the client.
+                    read_request = sr.message() => {
+                        let read_request = read_request
+                            .map_err(|e| SourceError(ErrorKind::InternalError(e.to_string())))?
+                            .ok_or_else(|| SourceError(ErrorKind::InternalError("Stream closed".to_string())))?;
+
+                        let request = read_request.request.ok_or_else(|| SourceError(ErrorKind::InternalError("Stream closed".to_string())))?;
+
+                        // start the ud-source rx asynchronously and start populating the gRPC
+                        // response, so it can be streamed to the gRPC client (numaflow).
+                        let grpc_resp_tx = grpc_tx.clone();
+
+                        // let's forward a batch for this request
+                        Self::forward_a_batch(handler_fn.clone(), grpc_resp_tx, request).await?
+                    }
+                    _ = cln_token.cancelled() => {
+                        info!("Cancellation token triggered, shutting down");
+                        break;
+                    }
+                }
             }
+            Ok(())
         });
 
-        let handler_fn = Arc::clone(&self.handler);
-        // we want to start streaming to the server as soon as possible
+        let shutdown_tx = self.shutdown_tx.clone();
+        // spawn so we can return the recv stream to client.
         tokio::spawn(async move {
-            // user-defined source read handler
-            handler_fn
-                .read(
-                    SourceReadRequest {
-                        count: sr.num_records as usize,
-                        timeout: Duration::from_millis(sr.timeout_in_ms as u64),
-                    },
-                    stx,
-                )
-                .await
+            // wait for the grpc read handle; if there are any errors, we set the gRPC Status to failure
+            // which will close the stream with failure.
+            if let Err(e) = grpc_read_handle.await {
+                error!("shutting down the gRPC channel, {}", e);
+                tx.send(Err(Status::internal(e.to_string())))
+                    .await
+                    .map_err(|e| SourceError(ErrorKind::InternalError(e.to_string())))
+                    .expect("writing error to grpc response channel should never fail");
+
+                // if there are any failures, we propagate those failures so that the server can shutdown.
+                shutdown_tx
+                    .send(())
+                    .await
+                    .expect("write to shutdown channel should never fail");
+            }
         });
 
         Ok(Response::new(ReceiverStream::new(rx)))
@@ -131,31 +244,29 @@ where
 
     async fn ack_fn(
         &self,
-        request: Request<proto::AckRequest>,
-    ) -> Result<Response<proto::AckResponse>, Status> {
-        let ar: proto::AckRequest = request.into_inner();
+        request: Request<Streaming<AckRequest>>,
+    ) -> Result<Response<AckResponse>, Status> {
+        let mut ack_stream = request.into_inner();
+        while let Some(ack_request) = ack_stream.message().await? {
+            // the request is not there send back status as invalid argument
+            let Some(request) = ack_request.request else {
+                return Err(Status::invalid_argument("request is empty"));
+            };
 
-        let success_response = Response::new(proto::AckResponse {
+            let Some(offset) = request.offset else {
+                return Err(Status::invalid_argument("offset is not present"));
+            };
+
+            self.handler
+                .ack(Offset {
+                    offset: offset.clone().offset,
+                    partition_id: offset.partition_id,
+                })
+                .await;
+        }
+        Ok(Response::new(AckResponse {
             result: Some(proto::ack_response::Result { success: Some(()) }),
-        });
-
-        let Some(request) = ar.request else {
-            return Ok(success_response);
-        };
-
-        // invoke the user-defined source's ack handler
-        let offsets = request
-            .offsets
-            .into_iter()
-            .map(|so| Offset {
-                offset: so.offset,
-                partition_id: so.partition_id,
-            })
-            .collect();
-
-        self.handler.ack(offsets).await;
-
-        Ok(success_response)
+        }))
     }
 
     async fn pending_fn(&self, _: Request<()>) -> Result<Response<proto::PendingResponse>, Status> {
@@ -276,8 +387,8 @@ impl<T> Server<T> {
 
         let source_service = SourceService {
             handler: Arc::new(handler),
-            _shutdown_tx: internal_shutdown_tx,
-            _cancellation_token: cln_token.clone(),
+            shutdown_tx: internal_shutdown_tx,
+            cancellation_token: cln_token.clone(),
         };
 
         let source_svc = proto::source_server::SourceServer::new(source_service)
@@ -318,18 +429,19 @@ impl<C> Drop for Server<C> {
 #[cfg(test)]
 mod tests {
     use super::{proto, Message, Offset, SourceReadRequest};
+    use crate::source;
     use chrono::Utc;
     use std::collections::{HashMap, HashSet};
+    use std::error::Error;
+    use std::time::Duration;
     use std::vec;
-    use std::{error::Error, time::Duration};
-
-    use crate::source;
     use tempfile::TempDir;
     use tokio::net::UnixStream;
     use tokio::sync::mpsc::Sender;
-    use tokio::sync::oneshot;
-    use tokio_stream::StreamExt;
+    use tokio::sync::{mpsc, oneshot};
+    use tokio_stream::wrappers::ReceiverStream;
     use tonic::transport::Uri;
+    use tonic::Request;
     use tower::service_fn;
     use uuid::Uuid;
 
@@ -377,13 +489,11 @@ mod tests {
             self.yet_to_ack.write().unwrap().extend(message_offsets)
         }
 
-        async fn ack(&self, offsets: Vec<Offset>) {
-            for offset in offsets {
-                self.yet_to_ack
-                    .write()
-                    .unwrap()
-                    .remove(&String::from_utf8(offset.offset).unwrap());
-            }
+        async fn ack(&self, offset: Offset) {
+            self.yet_to_ack
+                .write()
+                .unwrap()
+                .remove(&String::from_utf8(offset.offset).unwrap());
         }
 
         async fn pending(&self) -> usize {
@@ -433,83 +543,69 @@ mod tests {
             .await?;
 
         let mut client = proto::source_client::SourceClient::new(channel);
-        let request = tonic::Request::new(proto::ReadRequest {
+
+        // Test read_fn with bidirectional streaming
+        let (read_tx, read_rx) = mpsc::channel(4);
+        let read_request = proto::ReadRequest {
             request: Some(proto::read_request::Request {
                 num_records: 5,
-                timeout_in_ms: 500,
+                timeout_in_ms: 1000,
             }),
-        });
+        };
+        read_tx.send(read_request).await.unwrap();
+        drop(read_tx); // Close the sender to indicate no more requests
 
-        let resp = client.read_fn(request).await?;
-        let resp = resp.into_inner();
-        let result: Vec<proto::read_response::Result> = resp
-            .map(|item| item.unwrap().result.unwrap())
-            .collect()
-            .await;
-        let response_values: Vec<usize> = result
-            .iter()
-            .map(|item| {
-                usize::from_le_bytes(
-                    item.payload
-                        .clone()
-                        .try_into()
-                        .expect("expected Vec length to be 8"),
-                )
-            })
-            .collect();
-        assert_eq!(response_values, vec![8, 8, 8, 8, 8]);
-
-        let pending_before_ack = client
-            .pending_fn(tonic::Request::new(()))
-            .await
-            .unwrap()
+        let mut response_stream = client
+            .read_fn(Request::new(ReceiverStream::new(read_rx)))
+            .await?
             .into_inner();
-        assert_eq!(
-            pending_before_ack.result.unwrap().count,
-            5,
-            "Expected pending messages to be 5 before ACK"
-        );
+        let mut response_values = Vec::new();
 
-        let offsets_to_ack: Vec<proto::Offset> = result
-            .iter()
-            .map(|item| item.clone().offset.unwrap())
-            .collect();
-        let ack_request = tonic::Request::new(proto::AckRequest {
-            request: Some(proto::ack_request::Request {
-                offsets: offsets_to_ack,
-            }),
-        });
-        let resp = client.ack_fn(ack_request).await.unwrap().into_inner();
-        assert!(
-            resp.result.unwrap().success.is_some(),
-            "Expected acknowledgement request to be successful"
-        );
+        while let Some(response) = response_stream.message().await? {
+            if let Some(status) = response.status {
+                if status.eot {
+                    break;
+                }
+            }
 
-        let pending_before_ack = client
-            .pending_fn(tonic::Request::new(()))
-            .await
-            .unwrap()
+            if let Some(result) = response.result {
+                response_values.push(result);
+            }
+        }
+        assert_eq!(response_values.len(), 5);
+
+        // Test pending_fn
+        let pending_before_ack = client.pending_fn(Request::new(())).await?.into_inner();
+        assert_eq!(pending_before_ack.result.unwrap().count, 5);
+
+        // Test ack_fn with client-side streaming
+        let (ack_tx, ack_rx) = mpsc::channel(10);
+        for resp in response_values.iter() {
+            let ack_request = proto::AckRequest {
+                request: Some(proto::ack_request::Request {
+                    offset: Some(proto::Offset {
+                        offset: resp.offset.clone().unwrap().offset,
+                        partition_id: resp.offset.clone().unwrap().partition_id,
+                    }),
+                }),
+            };
+            ack_tx.send(ack_request).await.unwrap();
+        }
+        drop(ack_tx); // Close the sender to indicate no more requests
+
+        let ack_response = client
+            .ack_fn(Request::new(ReceiverStream::new(ack_rx)))
+            .await?
             .into_inner();
-        assert_eq!(
-            pending_before_ack.result.unwrap().count,
-            0,
-            "Expected pending messages to be 0 after ACK"
-        );
+        assert!(ack_response.result.unwrap().success.is_some());
 
-        let partitions = client
-            .partitions_fn(tonic::Request::new(()))
-            .await
-            .unwrap()
-            .into_inner();
-        assert_eq!(
-            partitions.result.unwrap().partitions,
-            vec![2],
-            "Expected number of partitions to be 2"
-        );
+        let pending_after_ack = client.pending_fn(Request::new(())).await?.into_inner();
+        assert_eq!(pending_after_ack.result.unwrap().count, 0);
 
-        shutdown_tx
-            .send(())
-            .expect("Sending shutdown signal to gRPC server");
+        let partitions = client.partitions_fn(Request::new(())).await?.into_inner();
+        assert_eq!(partitions.result.unwrap().partitions, vec![2]);
+
+        shutdown_tx.send(()).unwrap();
         tokio::time::sleep(Duration::from_millis(50)).await;
         assert!(task.is_finished(), "gRPC server is still running");
         Ok(())
