d36011abe0e906738422bc94ead4ef531d2f5609
Sreekanth <prsreekanth920@gmail.com>
2024-11-11 23:59:33 +0530

    chore: Use cargo workspaces (#102)
    Signed-off-by: Sreekanth <prsreekanth920@gmail.com>

--- examples/mapt-event-time-filter/src/main.rs
+++ examples/mapt-event-time-filter/src/main.rs
@@ -43,7 +43,7 @@ mod filter_impl {
 mod tests {
     use crate::filter_impl::filter_event_time;
     use chrono::{TimeZone, Utc};
-    use numaflow::sourcetransform::{Message, SourceTransformRequest};
+    use numaflow::sourcetransform::SourceTransformRequest;
     /// Tests that events from 2022 are tagged as within the year 2022.
     #[test]
     fn test_filter_event_time_should_return_within_year_2022() {
