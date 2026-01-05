d8651433a124814a9d54d8a5881d71d838ce0d91
harshithsaiv <harshithsai94@gmail.com>
2026-01-04 16:52:23 -0800

    test: Add unit tests for map-cat
    Signed-off-by: godfather2327 <harshithsai94@gmail.com>

--- examples/map-cat/src/main.rs
+++ examples/map-cat/src/main.rs
@@ -17,3 +17,85 @@ impl map::Mapper for Cat {
         vec![map::Message::new(input.value).with_keys(input.keys.clone())]
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use numaflow::map::{MapRequest, Mapper, SystemMetadata, UserMetadata};
+    use std::collections::HashMap;
+
+    fn create_request(keys: Vec<String>, value: Vec<u8>) -> MapRequest {
+        MapRequest {
+            keys,
+            value,
+            watermark: chrono::Utc::now(),
+            eventtime: chrono::Utc::now(),
+            headers: HashMap::new(),
+            user_metadata: UserMetadata::new(),
+            system_metadata: SystemMetadata::new(),
+        }
+    }
+
+    #[tokio::test]
+    async fn test_map_cat_passes_through_value() {
+        let cat = Cat;
+        let request = create_request(vec!["key1".to_string()], b"Hello, World!".to_vec());
+
+        let messages = cat.map(request).await;
+
+        assert_eq!(messages.len(), 1);
+        assert_eq!(messages[0].value, b"Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn test_map_cat_preserves_keys() {
+        let cat = Cat;
+        let request = create_request(
+            vec!["key1".to_string(), "key2".to_string()],
+            b"test".to_vec(),
+        );
+
+        let messages = cat.map(request).await;
+
+        assert_eq!(messages.len(), 1);
+        assert_eq!(
+            messages[0].keys,
+            Some(vec!["key1".to_string(), "key2".to_string()])
+        );
+    }
+
+    #[tokio::test]
+    async fn test_map_cat_empty_value() {
+        let cat = Cat;
+        let request = create_request(vec!["key1".to_string()], vec![]);
+
+        let messages = cat.map(request).await;
+
+        assert_eq!(messages.len(), 1);
+        assert_eq!(messages[0].value, Vec::<u8>::new());
+    }
+
+    #[tokio::test]
+    async fn test_map_cat_empty_keys() {
+        let cat = Cat;
+        let request = create_request(vec![], b"value".to_vec());
+
+        let messages = cat.map(request).await;
+
+        assert_eq!(messages.len(), 1);
+        assert_eq!(messages[0].keys, Some(vec![]));
+        assert_eq!(messages[0].value, b"value");
+    }
+
+    #[tokio::test]
+    async fn test_map_cat_binary_data() {
+        let cat = Cat;
+        let binary_data = vec![0x00, 0x01, 0xff, 0xfe, 0x10];
+        let request = create_request(vec!["key1".to_string()], binary_data.clone());
+
+        let messages = cat.map(request).await;
+
+        assert_eq!(messages.len(), 1);
+        assert_eq!(messages[0].value, binary_data);
+    }
+}
