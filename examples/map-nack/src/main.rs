470925cef0caa45904861ca749fdacb30e6f8dab
Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>
2026-07-18 11:46:04 -0400

    chore: examples for nacking at source, udf, and sink
    Signed-off-by: Vaibhav Tiwari <vaibhav.tiwari33@gmail.com>

---
+++ examples/map-nack/src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+use numaflow::map;
+use numaflow::shared::grpc_server::ServerExtras;
+use numaflow::shared::NackOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    map::Server::new(NackCat)
+        .with_max_message_size(10240)
+        .start()
+        .await
+}
+
+struct NackCat;
+
+#[tonic::async_trait]
+impl map::Mapper for NackCat {
+    async fn map(&self, input: map::MapRequest) -> Vec<map::Message> {
+        let mut nack_map = HashMap::new();
+        nack_map.insert("key".to_string(), "value".to_string());
+        let nack_options = NackOptions{
+            reason: Some("nacked in udf".to_string()),
+            nack_map,
+            ..Default::default()
+        };
+        vec![map::Message::message_to_nack(Some(nack_options))]
+    }
+}
